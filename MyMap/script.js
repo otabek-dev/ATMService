9ca65b3bd2ead9e0376e21bc67b3738b47e65303
Otabek Rustamov <otabek.pro@hotmail.com>
2023-10-12 16:11:10 +0500

    Test leaflet

---
+++ MyMap/script.js
@@ -0,0 +1,43 @@
+// let map = L.map('map', { doubleClickZoom: false }).locate({ setView: true, maxZoom: 16 });
+// // var map = L.map('map').setView([41.319083, 69.222635], 20);
+
+// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     maxZoom: 19,
+//     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+// }).addTo(map);
+
+var map = L.map('map').fitWorld();
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 20,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+
+map.locate({ setView: true, maxZoom: 16 });
+
+function onLocationFound(e) {
+    var radius = e.accuracy;
+
+    L.marker(e.latlng).addTo(map)
+        .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+    L.circle(e.latlng, radius).addTo(map);
+}
+
+map.on('locationfound', onLocationFound);
+
+function onLocationError(e) {
+    alert(e.message);
+}
+
+map.on('locationerror', onLocationError);
+
+var ATMs = L.layerGroup([
+    L.marker([41.309760, 69.274823]).bindPopup('This is ATM.'), 
+    L.marker([41.309286, 69.279196]).bindPopup('This is ATM'), 
+    L.marker([41.308191, 69.279333]).bindPopup('This is ATM'), 
+    L.marker([41.307503, 69.279023]).bindPopup('This is ATM'),
+    L.marker([41.308503, 69.279123]).bindPopup('This is ATM')
+]);
+
+ATMs.addTo(map);
